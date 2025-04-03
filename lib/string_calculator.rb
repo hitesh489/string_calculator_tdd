ac79fd0c5b199267b23ea31affba460876e7a6d6
Hitesh Chaudhari <hitesh.chaudhari@coupa.com>
2025-04-03 20:20:34 +0530

    Added tests for empty simple string of numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
-
 class StringCalculator
+  def add(numbers)
+  end
 end
---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,19 @@
+require 'string_calculator'
+
+describe StringCalculator do
+  let(:calculator) { StringCalculator.new }
+
+  describe "#add" do
+    it "returns 0 for an empty string" do
+      expect(calculator.add("")).to eq(0)
+    end
+
+    it "returns the number for a single number" do
+      expect(calculator.add("5")).to eq(5)
+    end
+
+    it "returns the sum of two numbers" do
+      expect(calculator.add("1,2")).to eq(3)
+    end
+  end
+end
