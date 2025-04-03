4c531ea008d672a133a45eb15c1ea6965335be69
Hitesh Chaudhari <hitesh.chaudhari@coupa.com>
2025-04-03 20:38:58 +0530

    Handles multiple numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -23,5 +23,9 @@ describe StringCalculator do
     it "returns the sum of two numbers" do
       expect(calculator.add("1,2")).to eq(3)
     end
+
+    it "returns the sum of multiple numbers" do
+      expect(calculator.add("1,2,3,4")).to eq(10)
+    end
   end
 end
